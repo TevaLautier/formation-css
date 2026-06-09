7c5eb5d66e40a767a2e7caf6207b4c9e7ee82f40
Teva Lautier <teva.lautier@soprasteria.com>
2026-06-09 17:45:21 +0200

    feat: day-1

--- src/app/modules/header/header/header.component.ts
+++ src/app/modules/header/header/header.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, HostBinding } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  Component,
+  OnInit,
+  HostBinding,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatIconModule } from "@angular/material/icon";
 import { RouterLink, RouterLinkActive } from "@angular/router";
@@ -6,15 +13,30 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, FormsModule, MatIconModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    FormsModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"]
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild("searchInput") inputSearch?: ElementRef;
+
   searchActive = false;
 
   search = "";
   constructor() {}
 
   ngOnInit() {}
+
+  flipSearch() {
+    this.searchActive = !this.searchActive;
+    if (this.searchActive) {
+      this.inputSearch?.nativeElement.focus();
+    }
+  }
 }
